c34326f1cce1d313c1de014b404700aa832683e4
Diego Alonso <diegoalonso872@gmail.com>
2024-11-12 21:18:53 -0600

    updated input data

--- Hands-on-2/simpleLinearRegression.py
+++ Hands-on-2/simpleLinearRegression.py
@@ -31,8 +31,8 @@ class SimpleLinearRegression:
             print(f"El valor de ventas estimado es: {valorEstimado:.2f}")
 
 #Datos hardcodeados
-x = [23,26,30,34,43,48,52,57,58]
-y = [651,762,856,1063,1190,1298,1421,1440,1518]
+x = [1,2,3,4,5,6,7,8,9]
+y = [2,4,6,8,10,12,14,16,18]
 
 #Creamos e inicializamos el modelo
 modelo = SimpleLinearRegression(x,y)
